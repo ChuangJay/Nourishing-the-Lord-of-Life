b9cc119764a0ee5cb7a6f8df09dbdc57c074afc4
Jay Chuang <jay.chuang@tpisoftware.com>
2021-11-29 23:41:03 +0800

    🎉

---
+++ src/main/java/com/example/laodan/LaodanApplication.java
@@ -0,0 +1,13 @@
+package com.example.laodan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaodanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaodanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/laodan/LaodanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laodan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaodanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
